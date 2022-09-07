98acbbaa1acb4e57d45ad7c6f9ce3a957f6fa8da
Alexander Ordinartsev <a.ordinartsev@gmail.com>
2022-09-08 02:16:22 +0300

    Lesson_2 Hello world with CBV and FBV

---
+++ mainapp/urls.py
@@ -0,0 +1,12 @@
+  GNU nano 4.8                        urls.py
+from django.http import HttpResponse
+from django.views.generic import View
+
+
+class HelloWorldView(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse("Hello world")
+
+
+def check_kwargs(request, **kwargs):
+    return HttpResponse(f"kwargs:<br>{kwargs}")
